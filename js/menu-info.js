d404b950d6c490101cef53cd56d7ea6f868175bf
Mei <ms840809@dal.ca>
2023-02-08 21:56:32 -0400

    first commit

---
+++ js/menu-info.js
@@ -0,0 +1,269 @@
+
+const soupSalad = [
+    {
+        name: "MISO SOUP",
+        price: "2.00"
+    },
+    {
+        name: "SPICY MISO SOUP",
+        price: "3.00"
+    },
+    {
+        name: "WONTON SOUP",
+        price: "5.95"
+    },
+    {
+        name: "FRESH SEAWEEN SALAD",
+        price: "5.75"
+    },
+    {
+        name: "GREEN SALAD",
+        price: "2.50"
+    },
+    {
+        name: "KANI SALAD",
+        price: "5.95"
+    }
+
+]
+
+const appetizers = [
+    {
+        name: "SPRING ROLL",
+        price: "6.45",
+        description: "3pcs served with sauce"
+    },
+    {
+        name: "FRIED WONTON (8pcs)",
+        price: "6.50"
+    },
+    {
+        name: "EDAMAME",
+        price: "5.95"
+    },
+    {
+        name: "GYOZA",
+        price: "6.95",
+        description: "5pcs shrimp tempura, 5pcs vegetable tempura"
+    },
+    {
+        name: "EGG ROLL",
+        price: "2.00",
+    },
+    {
+        name: "TEMPURA APPETIZERS",
+        price: "8.95",
+        description: "2pcs shrimp tempura, 5pcs vegetable tempura"
+    },
+    {
+        name: "YAM TEMPURA",
+        price: "6.95",
+        description: "5 pcs fried sweet potato"
+    },
+    {
+        name: "Shrimp Tempura",
+        price: "8.95",
+        description: "5 pcs fried shrimp tempura"
+    }
+]
+
+const nigiri = [
+
+    {
+        name: "Tuna Nigiri",
+        price: "6.75"
+    },
+    {
+        name: "Salmon Nirigi",
+        price: "Hamachi Nigiri"
+    },
+    {
+        name: "Hamachi Nigiri",
+        price: "6.25"
+    },
+    {
+        name: "Unagi Nigiri",
+        price: "7.25"
+    },
+    {
+        name: "Ika Nigiri",
+        price: "6.00",
+        description: "squid"
+    },
+    {
+        name: "Tako Nigiri",
+        price: "6.25",
+        description: "Octopus"
+    },
+    {
+        name: "Snapper Nigiri",
+        price: "6.00"
+    },
+    {
+        name: "Ebi",
+        price: "6.00",
+        description: "Shrimp"
+    },
+    {
+        name: "Kani",
+        price: "6.00",
+        description: "Immitation Crab"
+    }
+]
+
+const sashimi = [
+
+    {
+        name: "Tuna Sashimi",
+        price: "7.25"
+    },
+    {
+        name: "Salmon",
+        price: "6.75"
+    },
+    {
+        name: "Hamachi",
+        price: "8.25"
+    },
+    {
+        name: "Unagi",
+        price: "7.75"
+    },
+    {
+        name: "Ika",
+        price: "6.75",
+        description: "Squid"
+    },
+    {
+        name: "Tako",
+        price: "6.75",
+        description: "Octopus"
+    }
+]
+
+const makiRolls = [
+
+    {
+        name: "DYNAMITE ROLL",
+        price: "9.50",
+        description: "shrimp tempura, cucumber, avocado and mayo, topped with eel sauce"
+    },
+    {
+        name: "Deep Fried Salmon Roll",
+        price: "8.95",
+    },
+    {
+        name: "Spicy Tuna",
+        price: "7.75",
+        description: "tuna, avocado, tempura bits, spicy mayo"
+    },
+    {
+        name: "Spicy Salmon",
+        pric: "7.35",
+        description: "salmon, avocado, tempura bits, spicy mayo"
+    },
+    {
+        name: "Spicy Crispy",
+        price: "6.25",
+        description: "tempura bits, avocado, spicy mayo"
+    },
+    {
+        name: "Unagi Maki",
+        price: "8.25",
+        description: "bbq eel, avocado, cucumber, bbq eel sauce"
+    },
+    {
+        name: "Negi Hamachi",
+        price: "7.25",
+        description: "minced hamachi and green onion"
+    },
+    {
+        name: "Philadelphia",
+        price: "8.95",
+        description: "smoked salmon with cream cheese"
+    },
+    {
+        name: "California",
+        price: "6.00",
+        description: "cucumber, tobiko, kani, avocado"
+    },
+    {
+        name: "Sweet Potato",
+        price: "6.00",
+        description: "yam tempura, mayo, and avocado topped with eel sauce"
+    }, 
+    {
+        name: "Mango",
+        price: "6.75",
+        description: "mango and avocado"
+    },
+    {
+        name: "Cucumber and Avocado",
+        price: "6.75",
+    },
+    {
+        name: "Spicy Kani",
+        price: "6.75",
+        description: "avocado, tempura bits and spicy mayo"
+    },
+    {
+        name: "Salmon Maki",
+        price: "6.75",
+    },
+    {
+        name: "Tuna Maki",
+        price: "6.75",
+    },
+    {
+        name: "Cucumber Maki",
+        price: "4.25"
+    },
+    {
+        name: "Avocado Maki",
+        price: "4.25"
+    }
+]
+
+const sushiMakiCombos = [
+
+    {
+        name: "Silver Sushi",
+        price: "14.95",
+        description1: "1 pcs each nigiri: tuna, salmon, snapper, shrimp, kani, salmon hand roll",
+        description2: "3 pcs each maki roll: california and cucumber maki"
+    },
+    {
+        name: "Gold Sushi",
+        price: "17.50",
+        description1: "1 pcs each nigiri: tuna, salmon, snapper, kani, octopus, squid and salmon hand roll",
+        description2: "3 pcs each maki roll: california and salmon maki"
+    },
+    {
+        name: "Premium Sushi",
+        price: "21.95",
+        description1: "1 pcs each: tuna, salmon, snapper, kani, octopus, squid, hamachi, unagi and california hand roll",
+        description2: "3 pcs each maki roll: california and tuna maki"
+    },
+    {
+        name: "Salmon Delight",
+        price: "14.95",
+        description1: "4 pcs each: salmon nigiri",
+        description2: "6 pcs each maki roll: spicy salmon roll"
+    },
+    {
+        name: "Salmon Special",
+        price: "19.95",
+        description1: "4 pcs each: salmon nigiri and salmon sashimi",
+        description2: "6 pcs each: spicy salmon roll"
+    },
+    {
+        name: "Mixed Set A",
+        price: "13.50",
+        description1: "6 pcs each maki roll: sweet potato, cucumber, and avocado",
+    },
+    {
+        name: "Mixed Set B",
+        price: ""
+    }
+]
+
